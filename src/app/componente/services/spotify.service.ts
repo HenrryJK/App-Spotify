e1f2160f06338f325bb0f9b75cc4f901718a2a7c
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-04 09:53:31 -0500

    #7 Optimizar Peticiones del Serice Spotify

--- src/app/componente/services/spotify.service.ts
+++ src/app/componente/services/spotify.service.ts
@@ -10,26 +10,39 @@ export class SpotifyService {
     console.log('Spotify service listo!!!!!!!');
    }
 
-   getNewAlbum(){
-    const headers = new HttpHeaders({
-        'Authorization' : 'Bearer BQBT0t1oy4cqEUiWVjRWOcBG6FcwNAfRqa8y5QkZK3Rcmn9HAhrmO_oi8Q_Gan0WhBoGekyvmkSiPG-olEw'
-    });
+   getQuery(query : string){
+    const url = `https://api.spotify.com/v1/${query}`;
 
-   return  this.http.get('https://api.spotify.com/v1/browse/new-releases' , {headers})
-   .pipe(map (data => {
+    const headers = new HttpHeaders({
+      'Authorization' : 'Bearer BQAMTGwl65OqrUPbVeh-IfPI44dCbD8T2iNiZAVL6ym9YFMbGr7eHGmR_sLN_uj95SOTWGZGxBEvyIfG2VI'
+  });
+    return this.http.get(url , {headers});
+   }
+
+
+   getNewAlbum(){
+  /*  const headers = new HttpHeaders({
+        'Authorization' : 'Bearer BQBT0t1oy4cqEUiWVjRWOcBG6FcwNAfRqa8y5QkZK3Rcmn9HAhrmO_oi8Q_Gan0WhBoGekyvmkSiPG-olEw'
+    });*/
+
+   return  this.getQuery('browse/new-releases')
+    .pipe(map (data => {
       return data['albums'].items;
    }));
+   // this.http.get('https://api.spotify.com/v1/browse/new-releases' , {headers})
+
   }
 
-
   getArtist(termino:string){
-    const headers = new HttpHeaders({
+   /* const headers = new HttpHeaders({
       'Authorization' : 'Bearer BQBT0t1oy4cqEUiWVjRWOcBG6FcwNAfRqa8y5QkZK3Rcmn9HAhrmO_oi8Q_Gan0WhBoGekyvmkSiPG-olEw'
-  });
+  });*/
 
- return  this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=10` , {headers})
-  .pipe( map ( data => {
+ return   this.getQuery(`search?q=${termino}&type=artist&limit=10`)
+ .pipe( map ( data => {
     return data['artists'].items;
   }));
+// this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=10` , {headers})
+
   }
 }
