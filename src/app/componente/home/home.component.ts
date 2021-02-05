97d087b123e9337dfb1c552af55bda7d621c6897
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-05 14:24:48 -0500

    #15 Manejo de errores de un observable

--- src/app/componente/home/home.component.ts
+++ src/app/componente/home/home.component.ts
@@ -11,14 +11,21 @@ export class HomeComponent implements OnInit {
   canciones:any[] = [];
 
   loading: boolean;
-
+  error : boolean;
+  mensajeError: string;
   constructor(private spotify:SpotifyService) {
     this.loading = true;
+    this.error = false;
     this.spotify.getNewAlbum()
     .subscribe( (data:any) => {
 
       this.canciones=data;
       this.loading = false;
+      //caso contrario vota un error
+    } , (errorServicio) => {
+      this.error=true;
+      console.log(errorServicio);
+     this.mensajeError = errorServicio.error.error.message;
     });
   }
 
